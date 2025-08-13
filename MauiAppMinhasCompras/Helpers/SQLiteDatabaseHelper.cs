8c0b83eddef3ff88310a083cff2658d9e2ec5c0a
Matheus Costa <contato.matheusccp5@gmail.com>
2025-08-13 00:19:02 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,41 @@
+﻿using SQLite;
+using MauiAppMinhasCompras.Models;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn .CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> insertProduto(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> UpdateProduto(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id =?";
+            return _conn.QueryAsync<p>(
+                sql p.Descricao, p.Quantidade, p.Preco, p.Id
+                );
+        }
+        public Task <int> Delete(int id)
+        {
+            return _conn.Table<Produto>()
+                .Where(p => p.Id == id)
+                .DeleteAsync();
+        }
+        public Task <List<Produto>> GetAll() {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+        public Task<List<Produto>> Search (string q)
+        {
+            string sql = "SELECT * Produto WHERE Descricao '%" + q + "%'";
+            return _conn.QueryAsync<p>(sql);
+        }
+    }
+}
